17c6163c0ae1f194df1a45dbca730c5e55351b3a
goofy-wu <16620188468@163.com>
2025-04-11 15:56:48 +0800

    Create Hello World.sol

---
+++ Hello World.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT 
+//声明协议
+pragma solidity ^0.8.24; //版本
+
+//定义合约 contract 合约名
+contract HelloWrold{
+    //创建合约变量 类型+可视范围+变量名 = 变量值
+    //public可视范围：公开
+    string public myString = "hello world";
+}
+
+//1、点击deploy部署合约
+//2、合约蓝色按钮代表只读
